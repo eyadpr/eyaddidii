0fbd4eb1d768d4001098b265144c23563e4e99cc
eyadpr <105747874+eyadpr@users.noreply.github.com>
2023-10-05 18:37:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var menuicon = document.getElementById("menu-item");
+var list = document.getElementById("list1");
+var menustatus;
+var leftarrow = document.getElementById("left");
+var rightarrow = document.getElementById("right");
+var slider = document.getElementById("maincontent");
+var images = ["slide-1.jpg","post2.jpg","post3.jpg"];
+var i;
+i = 0;
+
+
+
+menustatus = "closed";
+
+menuicon.onclick =  function() {
+    if(menustatus == "closed"){
+       menuicon.src = "close.png";
+        console.log(list);
+    list.style.display = "block";
+    menustatus = "open";
+    } else{
+       menuicon.src = "menu.png";
+    list.style.display = "none";
+    menustatus = "closed";
+    }
+};
+
+
+window.onscroll = function() {
+    menuicon.src = "menu.png";
+    list.style.display = "none";
+    menustatus = "close";
+    
+    if(window.pageYOffset > 0){
+       leftarrow.style.display = "none"; 
+       rightarrow.style.display = "none";
+        console.log("hello");
+        
+    } else {
+        leftarrow.style.display = "block";
+        rightarrow.style.display ="block";
+    }
+   
+}
+ 
+
+
+
+
+slider.style.backgroundImage = "url("+images[i]+")";
+
+leftarrow.onclick = function() {
+    
+    if(i>0){
+   slider.style.backgroundImage = "url("+images[i-1]+")";
+    i = i - 1;   
+}
+  
+};
+rightarrow.onclick = function() {
+  if(i<images.length-1){
+   slider.style.backgroundImage = "url("+images[i+1]+")";
+  i = i + 1;  
+}
+};
